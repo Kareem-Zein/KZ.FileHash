4ad7ff8b674eaf2fcffb2032e4876bd8b52dfd90
github-action-benchmark <github@users.noreply.github.com>
2026-08-22 04:18:54 +0000

    add KZ.FileHash Benchmark (benchmarkdotnet) benchmark result for c0bed165ab13d75b03f986d66c5a682f8b60d930

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1787367059391,
+  "lastUpdate": 1787372334256,
   "repoUrl": "https://github.com/Kareem-Zein/KZ.FileHash",
   "entries": {
     "KZ.FileHash Benchmark": [
@@ -102,6 +102,316 @@ window.BENCHMARK_DATA = {
             "value": 15925509.547916668,
             "unit": "ns",
             "range": "± 55517.63547397668"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Kareem Zein",
+            "username": "Kareem-Zein",
+            "email": "karkarem004@gmail.com"
+          },
+          "committer": {
+            "name": "Kareem Zein",
+            "username": "Kareem-Zein",
+            "email": "karkarem004@gmail.com"
+          },
+          "id": "c0bed165ab13d75b03f986d66c5a682f8b60d930",
+          "message": "Restore JsonExporterAttribute.",
+          "timestamp": "2026-08-22T03:42:45Z",
+          "url": "https://github.com/Kareem-Zein/KZ.FileHash/commit/c0bed165ab13d75b03f986d66c5a682f8b60d930"
+        },
+        "date": 1787372333390,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "KZ.FileHash.Benchmarks.FileHashBenchmarks.FileHashBenchmarks.KZFileHashSingleAlgorithm(FileSize: 1048576, BufferSize: 65536)",
+            "value": 1366062.969576322,
+            "unit": "ns",
+            "range": "± 36384.824247450546"
+          },
+          {
+            "name": "KZ.FileHash.Benchmarks.FileHashBenchmarks.FileHashBenchmarks.KZFileHashAllAlgorithms(FileSize: 1048576, BufferSize: 65536)",
+            "value": 12424181.352481619,
+            "unit": "ns",
+            "range": "± 253758.70870759708"
+          },
+          {
+            "name": "KZ.FileHash.Benchmarks.FileHashBenchmarks.FileHashBenchmarks.TraditionalReadAllBytesAsync(FileSize: 1048576, BufferSize: 65536)",
+            "value": 1709811.5368489583,
+            "unit": "ns",
+            "range": "± 22331.81741904049"
+          },
+          {
+            "name": "KZ.FileHash.Benchmarks.FileHashBenchmarks.FileHashBenchmarks.TraditionalReadAllBytesAsyncMultiAlgorithm(FileSize: 1048576, BufferSize: 65536)",
+            "value": 12780373.726041667,
+            "unit": "ns",
+            "range": "± 81365.24417635555"
+          },
+          {
+            "name": "KZ.FileHash.Benchmarks.FileHashBenchmarks.FileHashBenchmarks.TraditionalStreamWithComputeHashAsync(FileSize: 1048576, BufferSize: 65536)",
+            "value": 1431760.1264973958,
+            "unit": "ns",
+            "range": "± 6305.446654757284"
+          },
+          {
+            "name": "KZ.FileHash.Benchmarks.FileHashBenchmarks.FileHashBenchmarks.TraditionalStreamWithComputeHashAsyncMultiAlgorithm(FileSize: 1048576, BufferSize: 65536)",
+            "value": 14613631.715625,
+            "unit": "ns",
+            "range": "± 93863.11867373232"
+          },
+          {
+            "name": "KZ.FileHash.Benchmarks.FileHashBenchmarks.FileHashBenchmarks.KZFileHashSingleAlgorithm(FileSize: 1048576, BufferSize: 1048576)",
+            "value": 1289385.2996651786,
+            "unit": "ns",
+            "range": "± 2141.1931100532775"
+          },
+          {
+            "name": "KZ.FileHash.Benchmarks.FileHashBenchmarks.FileHashBenchmarks.KZFileHashAllAlgorithms(FileSize: 1048576, BufferSize: 1048576)",
+            "value": 13067850.173596399,
+            "unit": "ns",
+            "range": "± 560843.3025043727"
+          },
+          {
+            "name": "KZ.FileHash.Benchmarks.FileHashBenchmarks.FileHashBenchmarks.TraditionalReadAllBytesAsync(FileSize: 1048576, BufferSize: 1048576)",
+            "value": 1768051.2740119486,
+            "unit": "ns",
+            "range": "± 56781.06412682538"
+          },
+          {
+            "name": "KZ.FileHash.Benchmarks.FileHashBenchmarks.FileHashBenchmarks.TraditionalReadAllBytesAsyncMultiAlgorithm(FileSize: 1048576, BufferSize: 1048576)",
+            "value": 12913558.17299107,
+            "unit": "ns",
+            "range": "± 160472.30157620268"
+          },
+          {
+            "name": "KZ.FileHash.Benchmarks.FileHashBenchmarks.FileHashBenchmarks.TraditionalStreamWithComputeHashAsync(FileSize: 1048576, BufferSize: 1048576)",
+            "value": 1491298.2681300952,
+            "unit": "ns",
+            "range": "± 32224.91879305721"
+          },
+          {
+            "name": "KZ.FileHash.Benchmarks.FileHashBenchmarks.FileHashBenchmarks.TraditionalStreamWithComputeHashAsyncMultiAlgorithm(FileSize: 1048576, BufferSize: 1048576)",
+            "value": 14611509.787326388,
+            "unit": "ns",
+            "range": "± 486332.43623751873"
+          },
+          {
+            "name": "KZ.FileHash.Benchmarks.FileHashBenchmarks.FileHashBenchmarks.KZFileHashSingleAlgorithm(FileSize: 16777216, BufferSize: 65536)",
+            "value": 21834449.66517857,
+            "unit": "ns",
+            "range": "± 344276.35735817626"
+          },
+          {
+            "name": "KZ.FileHash.Benchmarks.FileHashBenchmarks.FileHashBenchmarks.KZFileHashAllAlgorithms(FileSize: 16777216, BufferSize: 65536)",
+            "value": 199608642.4,
+            "unit": "ns",
+            "range": "± 1841447.7460990762"
+          },
+          {
+            "name": "KZ.FileHash.Benchmarks.FileHashBenchmarks.FileHashBenchmarks.TraditionalReadAllBytesAsync(FileSize: 16777216, BufferSize: 65536)",
+            "value": 23332480.13125,
+            "unit": "ns",
+            "range": "± 171045.27168513715"
+          },
+          {
+            "name": "KZ.FileHash.Benchmarks.FileHashBenchmarks.FileHashBenchmarks.TraditionalReadAllBytesAsyncMultiAlgorithm(FileSize: 16777216, BufferSize: 65536)",
+            "value": 200033773.35555556,
+            "unit": "ns",
+            "range": "± 2201356.2305791182"
+          },
+          {
+            "name": "KZ.FileHash.Benchmarks.FileHashBenchmarks.FileHashBenchmarks.TraditionalStreamWithComputeHashAsync(FileSize: 16777216, BufferSize: 65536)",
+            "value": 22604742.441964287,
+            "unit": "ns",
+            "range": "± 124477.3456825802"
+          },
+          {
+            "name": "KZ.FileHash.Benchmarks.FileHashBenchmarks.FileHashBenchmarks.TraditionalStreamWithComputeHashAsyncMultiAlgorithm(FileSize: 16777216, BufferSize: 65536)",
+            "value": 223301311.83333334,
+            "unit": "ns",
+            "range": "± 2430700.380570607"
+          },
+          {
+            "name": "KZ.FileHash.Benchmarks.FileHashBenchmarks.FileHashBenchmarks.KZFileHashSingleAlgorithm(FileSize: 16777216, BufferSize: 1048576)",
+            "value": 21180567.135416668,
+            "unit": "ns",
+            "range": "± 360514.8632191012"
+          },
+          {
+            "name": "KZ.FileHash.Benchmarks.FileHashBenchmarks.FileHashBenchmarks.KZFileHashAllAlgorithms(FileSize: 16777216, BufferSize: 1048576)",
+            "value": 198797507.3589744,
+            "unit": "ns",
+            "range": "± 927240.5786164404"
+          },
+          {
+            "name": "KZ.FileHash.Benchmarks.FileHashBenchmarks.FileHashBenchmarks.TraditionalReadAllBytesAsync(FileSize: 16777216, BufferSize: 1048576)",
+            "value": 23707318.151442308,
+            "unit": "ns",
+            "range": "± 272770.6137128055"
+          },
+          {
+            "name": "KZ.FileHash.Benchmarks.FileHashBenchmarks.FileHashBenchmarks.TraditionalReadAllBytesAsyncMultiAlgorithm(FileSize: 16777216, BufferSize: 1048576)",
+            "value": 205480700.5606061,
+            "unit": "ns",
+            "range": "± 4529807.264562635"
+          },
+          {
+            "name": "KZ.FileHash.Benchmarks.FileHashBenchmarks.FileHashBenchmarks.TraditionalStreamWithComputeHashAsync(FileSize: 16777216, BufferSize: 1048576)",
+            "value": 23105871.770833332,
+            "unit": "ns",
+            "range": "± 233241.15327651348"
+          },
+          {
+            "name": "KZ.FileHash.Benchmarks.FileHashBenchmarks.FileHashBenchmarks.TraditionalStreamWithComputeHashAsyncMultiAlgorithm(FileSize: 16777216, BufferSize: 1048576)",
+            "value": 236618328.48039216,
+            "unit": "ns",
+            "range": "± 9531246.37434853"
+          },
+          {
+            "name": "KZ.FileHash.Benchmarks.FileHashBenchmarks.FileHashBenchmarks.KZFileHashSingleAlgorithm(FileSize: 104857600, BufferSize: 65536)",
+            "value": 145215904.80555555,
+            "unit": "ns",
+            "range": "± 4025575.756748772"
+          },
+          {
+            "name": "KZ.FileHash.Benchmarks.FileHashBenchmarks.FileHashBenchmarks.KZFileHashAllAlgorithms(FileSize: 104857600, BufferSize: 65536)",
+            "value": 1260990659.9473684,
+            "unit": "ns",
+            "range": "± 26616449.76200233"
+          },
+          {
+            "name": "KZ.FileHash.Benchmarks.FileHashBenchmarks.FileHashBenchmarks.TraditionalReadAllBytesAsync(FileSize: 104857600, BufferSize: 65536)",
+            "value": 150085515.80833334,
+            "unit": "ns",
+            "range": "± 3618954.8695176705"
+          },
+          {
+            "name": "KZ.FileHash.Benchmarks.FileHashBenchmarks.FileHashBenchmarks.TraditionalReadAllBytesAsyncMultiAlgorithm(FileSize: 104857600, BufferSize: 65536)",
+            "value": 1282056609.9333334,
+            "unit": "ns",
+            "range": "± 23833014.796491805"
+          },
+          {
+            "name": "KZ.FileHash.Benchmarks.FileHashBenchmarks.FileHashBenchmarks.TraditionalStreamWithComputeHashAsync(FileSize: 104857600, BufferSize: 65536)",
+            "value": 156568236.175,
+            "unit": "ns",
+            "range": "± 5446388.049164384"
+          },
+          {
+            "name": "KZ.FileHash.Benchmarks.FileHashBenchmarks.FileHashBenchmarks.TraditionalStreamWithComputeHashAsyncMultiAlgorithm(FileSize: 104857600, BufferSize: 65536)",
+            "value": 1500878862.9333334,
+            "unit": "ns",
+            "range": "± 44776327.36920294"
+          },
+          {
+            "name": "KZ.FileHash.Benchmarks.FileHashBenchmarks.FileHashBenchmarks.KZFileHashSingleAlgorithm(FileSize: 104857600, BufferSize: 1048576)",
+            "value": 135521474.69642857,
+            "unit": "ns",
+            "range": "± 2267775.0168367126"
+          },
+          {
+            "name": "KZ.FileHash.Benchmarks.FileHashBenchmarks.FileHashBenchmarks.KZFileHashAllAlgorithms(FileSize: 104857600, BufferSize: 1048576)",
+            "value": 1259120682.2272727,
+            "unit": "ns",
+            "range": "± 29965730.732287757"
+          },
+          {
+            "name": "KZ.FileHash.Benchmarks.FileHashBenchmarks.FileHashBenchmarks.TraditionalReadAllBytesAsync(FileSize: 104857600, BufferSize: 1048576)",
+            "value": 149727079.8653846,
+            "unit": "ns",
+            "range": "± 2467108.536202368"
+          },
+          {
+            "name": "KZ.FileHash.Benchmarks.FileHashBenchmarks.FileHashBenchmarks.TraditionalReadAllBytesAsyncMultiAlgorithm(FileSize: 104857600, BufferSize: 1048576)",
+            "value": 1258716552.142857,
+            "unit": "ns",
+            "range": "± 6049885.375274958"
+          },
+          {
+            "name": "KZ.FileHash.Benchmarks.FileHashBenchmarks.FileHashBenchmarks.TraditionalStreamWithComputeHashAsync(FileSize: 104857600, BufferSize: 1048576)",
+            "value": 149726186.28888887,
+            "unit": "ns",
+            "range": "± 596456.4013300055"
+          },
+          {
+            "name": "KZ.FileHash.Benchmarks.FileHashBenchmarks.FileHashBenchmarks.TraditionalStreamWithComputeHashAsyncMultiAlgorithm(FileSize: 104857600, BufferSize: 1048576)",
+            "value": 1463256299.8,
+            "unit": "ns",
+            "range": "± 18158926.91677062"
+          },
+          {
+            "name": "KZ.FileHash.Benchmarks.FileHashBenchmarks.FileHashBenchmarks.KZFileHashSingleAlgorithm(FileSize: 524288000, BufferSize: 65536)",
+            "value": 747647285.7666667,
+            "unit": "ns",
+            "range": "± 33391102.001156226"
+          },
+          {
+            "name": "KZ.FileHash.Benchmarks.FileHashBenchmarks.FileHashBenchmarks.KZFileHashAllAlgorithms(FileSize: 524288000, BufferSize: 65536)",
+            "value": 6406846568.066667,
+            "unit": "ns",
+            "range": "± 100959390.2301184"
+          },
+          {
+            "name": "KZ.FileHash.Benchmarks.FileHashBenchmarks.FileHashBenchmarks.TraditionalReadAllBytesAsync(FileSize: 524288000, BufferSize: 65536)",
+            "value": 745479716.0526316,
+            "unit": "ns",
+            "range": "± 15742726.825017292"
+          },
+          {
+            "name": "KZ.FileHash.Benchmarks.FileHashBenchmarks.FileHashBenchmarks.TraditionalReadAllBytesAsyncMultiAlgorithm(FileSize: 524288000, BufferSize: 65536)",
+            "value": 6414310337.071428,
+            "unit": "ns",
+            "range": "± 82101468.29329175"
+          },
+          {
+            "name": "KZ.FileHash.Benchmarks.FileHashBenchmarks.FileHashBenchmarks.TraditionalStreamWithComputeHashAsync(FileSize: 524288000, BufferSize: 65536)",
+            "value": 748177941.0833334,
+            "unit": "ns",
+            "range": "± 7183321.339447759"
+          },
+          {
+            "name": "KZ.FileHash.Benchmarks.FileHashBenchmarks.FileHashBenchmarks.TraditionalStreamWithComputeHashAsyncMultiAlgorithm(FileSize: 524288000, BufferSize: 65536)",
+            "value": 7554120107.333333,
+            "unit": "ns",
+            "range": "± 123429549.8379245"
+          },
+          {
+            "name": "KZ.FileHash.Benchmarks.FileHashBenchmarks.FileHashBenchmarks.KZFileHashSingleAlgorithm(FileSize: 524288000, BufferSize: 1048576)",
+            "value": 663759131.1428572,
+            "unit": "ns",
+            "range": "± 4498224.417740419"
+          },
+          {
+            "name": "KZ.FileHash.Benchmarks.FileHashBenchmarks.FileHashBenchmarks.KZFileHashAllAlgorithms(FileSize: 524288000, BufferSize: 1048576)",
+            "value": 6063890035.538462,
+            "unit": "ns",
+            "range": "± 28883203.51930072"
+          },
+          {
+            "name": "KZ.FileHash.Benchmarks.FileHashBenchmarks.FileHashBenchmarks.TraditionalReadAllBytesAsync(FileSize: 524288000, BufferSize: 1048576)",
+            "value": 727988138.4285715,
+            "unit": "ns",
+            "range": "± 1971088.9669262953"
+          },
+          {
+            "name": "KZ.FileHash.Benchmarks.FileHashBenchmarks.FileHashBenchmarks.TraditionalReadAllBytesAsyncMultiAlgorithm(FileSize: 524288000, BufferSize: 1048576)",
+            "value": 6330519689.857142,
+            "unit": "ns",
+            "range": "± 63323504.62620577"
+          },
+          {
+            "name": "KZ.FileHash.Benchmarks.FileHashBenchmarks.FileHashBenchmarks.TraditionalStreamWithComputeHashAsync(FileSize: 524288000, BufferSize: 1048576)",
+            "value": 739417003.4166666,
+            "unit": "ns",
+            "range": "± 7022369.567313022"
+          },
+          {
+            "name": "KZ.FileHash.Benchmarks.FileHashBenchmarks.FileHashBenchmarks.TraditionalStreamWithComputeHashAsyncMultiAlgorithm(FileSize: 524288000, BufferSize: 1048576)",
+            "value": 7359818998.266666,
+            "unit": "ns",
+            "range": "± 104488985.6766327"
           }
         ]
       }
