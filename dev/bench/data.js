85b65cf790fa7b6460971cd8d66b4d693fbca8c0
github-action-benchmark <github@users.noreply.github.com>
2026-08-22 02:36:53 +0000

    add KZ.FileHash Benchmark (benchmarkdotnet) benchmark result for a8bcbe71f32f07bff5bbeb2e2f04edba35d64cae

---
+++ dev/bench/data.js
@@ -0,0 +1,42 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1787366213353,
+  "repoUrl": "https://github.com/Kareem-Zein/KZ.FileHash",
+  "entries": {
+    "KZ.FileHash Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "Kareem Zein",
+            "username": "Kareem-Zein",
+            "email": "karkarem004@gmail.com"
+          },
+          "committer": {
+            "name": "Kareem Zein",
+            "username": "Kareem-Zein",
+            "email": "karkarem004@gmail.com"
+          },
+          "id": "a8bcbe71f32f07bff5bbeb2e2f04edba35d64cae",
+          "message": ".",
+          "timestamp": "2026-08-22T02:35:24Z",
+          "url": "https://github.com/Kareem-Zein/KZ.FileHash/commit/a8bcbe71f32f07bff5bbeb2e2f04edba35d64cae"
+        },
+        "date": 1787366212336,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "KZ.FileHash.Benchmarks.FileHashBenchmarks.FileHashBenchmarks.KZFileHashSingleAlgorithm(FileSize: 1048576, BufferSize: 65536)",
+            "value": 1703099.1520833333,
+            "unit": "ns",
+            "range": "± 17506.34505673048"
+          },
+          {
+            "name": "KZ.FileHash.Benchmarks.FileHashBenchmarks.FileHashBenchmarks.KZFileHashAllAlgorithms(FileSize: 1048576, BufferSize: 65536)",
+            "value": 15752619.183333334,
+            "unit": "ns",
+            "range": "± 121757.26636204848"
+          }
+        ]
+      }
+    ]
+  }
+}
